9b6bc0c394d82dbc8eff15b3c21333f513d8c57a
Carys Leung <caryshcleung@gmail.com>
2022-11-30 15:07:59 +0000

    edited version - able to run in react, but no output

--- src/components/LoginLocalStorage/check.js
+++ src/components/LoginLocalStorage/check.js
@@ -1,84 +1,111 @@
-import React from "react";
 
-export default function Checks() {
+//import axios from 'axios';
+
+export default function Checks(userEmail) {
     // Assume checks and the boolean from EmailCheck() is imported 
-    function AlertMessage (checks, EmailCheck()) {
-        var j = 1
-        var errorMessages = ["This E-mail is already associated with an existing account", "Password does not contain numbers", "Password does not contain capital letters", "Password does not contain lowercase letters", "Password must be at least 8 characters", "Password does not contain special characters (E.g. !@#$%^&*", "The passwords do not match each other"]
-        var alertMessage = []
-        return(
-            checks.unshift(EmailCheck())
-            if (checks.includes(false)){
-                while (j <= checks.length){
-                    if (checks.at(j) == false){
-                        alertMessage.splice(1,errorMessages.at(j))
-                    }
-                    j++;
+
+    // Mock data for testing
+
+    const mockList = [
+        "guy1@example.com",
+        "guy2@example.com",
+        "guy3@example.com"
+    ]
+    const email = "guy3@example.com"
+    const password = "abcABC5!?"
+    const confirmPassword = "abcABC5!?"
+
+    function AlertMessage() {
+        let j = 1
+        let errorMessages = ["This E-mail is already associated with an existing account", "Password does not contain numbers", "Password does not contain capital letters", "Password does not contain lowercase letters", "Password must be at least 8 characters", "Password does not contain special characters (E.g. !@#$%^&*", "The passwords do not match each other"]
+        let alertMessage = []
+
+        let checks = PasswordCheck()
+
+        checks.unshift(EmailCheck(userEmail));
+        if (checks.includes(false)){
+            while (j <= checks.length){
+                if (checks.at(j) === false){
+                    alertMessage.splice(1,errorMessages.at(j))
                 }
-                var alert = <ul><li>{alertMessage}</li></ul> // list of error alert messages for not meeting conditions for email and password
-                return alert(alert);
-            }else{
-                return alert("Sign up successful!");
+                j++;
             }
-        )
+            let displayMessage = "";
+            let message = "";
+            
+            for (message of alertMessage) {
+                displayMessage += `, ${message}`;
+            }
+
+            // list of error alert messages for not meeting conditions for email and password
+            return alert(displayMessage); // alert() only works with strings
+        }else{
+            return alert("Sign up successful!");
+        }
     }
 
-    function PasswordCheck(password, confirmPassword) {
+    function PasswordCheck() {
         var i = 0;
-        var specialChar = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+        var specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
         // check order: num, capital, lowercase, length, special characters, password match
         var checks = [false,false,false,false,false,false];
-        return(
-            while (i <= password.length){
-                character = password.charAt(i);
-                if (!isNaN(character * 1)){
-                    numCheck=true;
-                    checks.splice(1,1,numCheck)
-                }
-                if (character == character.toUpperCase()) {
-                    capCheck=true;
-                    checks.splice(2,1,capCheck)
-                }
-                if (character == character.toLowerCase()){
-                    lowCheck=true;
-                    checks.splice(3,1,lowCheck)
-                }
-                if (specialChar.test(character)){
-                    specialCheck=true;
-                    checks.splice(-2,1,specialCheck)
-                }
-                }
-                    
-                i++;
-            if (password == confirmPassword){
-                    passwordMatch = true
-                    checks.splice(-1,1,passwordMatch)
+
+        while (i <= password.length){
+            let character = password.charAt(i);
+            if (!isNaN(character * 1)){
+                let numCheck=true;
+                checks.splice(1,1,numCheck)
             }
-            if (password.length >= 8){
-                lengthCheck=true
-                checks.splice(4,1,lengthCheck)
+            if (character === character.toUpperCase()) {
+                let capCheck=true;
+                checks.splice(2,1,capCheck)
             }
-            return checks;
-            <AlertMessage />
-        )
+            if (character === character.toLowerCase()){
+                let lowCheck=true;
+                checks.splice(3,1,lowCheck)
+            }
+            if (specialChar.test(character)){
+                let specialCheck=true;
+                checks.splice(-2,1,specialCheck)
+            }
+            }
+                
+            i++;
+        if (password === confirmPassword){
+                const passwordMatch = true
+                checks.splice(-1,1,passwordMatch)
+        }
+        if (password.length >= 8){
+            const lengthCheck = true
+            checks.splice(4,1,lengthCheck)
+        }
+
+        return checks;
+        
     }
 
     //Assume email is the email input from the user
-    function EmailCheck(email) {
-        axios.get("abc.com").then(function(
-            response
-        ) {
-            return(
-                //Assume "response.data" turns "response" into array
-                // Checks if email entered by the user is within the database already
-                if (response.data.includes(email)) {
-                    return true;
-                } else {
-                    return false;
-                }
-                <PasswordCheck />
-            )
-        })
+
+    /**
+     * GET abc.com returns a list of email addresses registered
+     */
+    function EmailCheck() {
+        //axios.get("abc.com").then(function(
+            //response
+        //) {
+            //Assume "response.data" turns "response" into array
+            // Checks if email entered by the user is within the database already
+            //if (response.data.includes(email)) {
+            //    return false;
+            //} else {
+            //    return true;
+            //}
+            if (mockList.includes(email)) {
+                return false;
+            } else {
+                return true;
+            }
+        //})
     }
-    <EmailCheck />
+    <AlertMessage />
 }
--- src/components/LoginLocalStorage/create.js
+++ src/components/LoginLocalStorage/create.js
@@ -1,76 +1,15 @@
 import React from 'react';
-import axios from 'axios';
+
+import Checks from "./check"
 
 export default function Signup() {
-    //Assume password is the password input from the user
-    function PasswordCheck(password, confirmPassword) {
-        var i = 0;
-        var numCheck = false;
-        var capCheck = false;
-        var lowCheck = false;
-        var lengthCheck = false;
-        var specialCheck = false;
-        var specialChar = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-        var passwordMatch = false;
-        var errorMessages = ["Password does not contain numbers", "Password does not contain capital letters", "Password does not contain lowercase letters", "Password must be at least 8 characters", "Password does not contain special characters (E.g. !@#$%^&*", "The passwords do not match each other"]
-        return(
-            while (i <= password.length){
-                character = password.charAt(i);
-                if (!isNaN(character * 1)){
-                    numCheck=true;
-                }
-                else{
-                    if (character == character.toUpperCase()) {
-                        capCheck=true;
-                    }
-                    if (character == character.toLowerCase()){
-                        lowCheck=true;
-                    }
-                    if (specialChar.test(character)){
-                        specialCheck=true;
-                    }
-                    }
-                }    
-                i++;
-            }
-            if (password == confirmPassword){
-                passwordMatch=true;
-            }
-            if (password.length >= 8){
-                lengthCheck=true;
-            if (numCheck==true&&capCheck==true&&lowCheck==true&&lengthCheck==true&&specialCheck==true&&passwordMatch==true){
-                return true;
-            }
-            else{
-                if ()
-            }
-        )
-    }
-    //Assume email is the email input from the user
-    function EmailCheck(email) {
-        axios.get("abc.com").then(function(
-            response
-        ) {
-            return(
-                //Assume "response.data" turns "response" into array
-                // Checks if email entered by the user is within the database already
-                if (response.data.includes(email)) {
-                    alert("This email is already associated with an account");
-                } else {
-                    var check = <PasswordCheck />;
-                    if 
-                }
-            )
-        })
-    }
-    return(
+    return (
         <div>
             <h1>Signup</h1>
-            //get input from user
-            //Check if email in database
-            //password validation: length, numbers, capitalisations
             <form>
-                <div>
+                <div>{/*get input from user*/}
+                {/*Check if email in database*/}
+                {/*password validation: length, numbers, capitalisations*/}
                     <label>Email:</label>
                     <input placeholder="Enter email here"/>
                 </div>
@@ -83,7 +22,7 @@ export default function Signup() {
                     <input/>
                 </div>
             </form>
-            <button onclick="EmailCheck()">Sign up</button>
+            <button OnClick={Checks}>Sign up</button>
         </div>
     )
 }
